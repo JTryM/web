16d33e6ef049bc77008c70ad75011fba33943bf5
Carlos Daniel Marceleño Rodriguez <carlosd.marceleno@gmail.com>
2019-06-17 20:52:56 -0500

    Update index.js

--- scr/index.js
+++ scr/index.js
@@ -1,5 +1,5 @@
 
-const express = require('express');
+const express = require('express'); 
 const path = require('path');
 const exphbs = require('express-handlebars');
 const methodOverride = require('method-override');
