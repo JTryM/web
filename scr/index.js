ffabc83639c57c658beaeef8aa217aac04a89eb9
Carlos Daniel Marceleño Rodriguez <carlosd.marceleno@gmail.com>
2019-06-17 19:46:03 -0500

    Update index.js

--- scr/index.js
+++ scr/index.js
@@ -4,9 +4,11 @@ const path = require('path');
 const exphbs = require('express-handlebars');
 const methodOverride = require('method-override');
 const session = require('express-session');
+
 //Initalizations
 const app = express();
 require('./database');
+
 // Settings
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
@@ -31,8 +33,10 @@ app.use(session({
 app.use(require('./routes/index'));
 app.use(require('./routes/notes'));
 app.use(require('./routes/users'));
+
 //Static Files ()
 app.use(express.static(path.join(__dirname,'public')));
+
 //Server is listenning
 app.listen(app.get('port'),()=>{
     console.log('Server on port',app.get('port'));
